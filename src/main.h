359009d5f954b86c554202212f4826fa739c2c1e
Lauri Kasanen <curaga@operamail.com>
2015-02-21 16:39:56 +0200

    Include autoconfig.h, depend on main.h

--- src/main.h
+++ src/main.h
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "autoconfig.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
