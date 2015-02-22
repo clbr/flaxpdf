1b0c5ac8d65eb296c5fadba04e164e70db01a8bd
Lauri Kasanen <curaga@operamail.com>
2015-02-22 21:52:29 +0200

    This seems to create an acceptable XImage

--- src/view.cpp
+++ src/view.cpp
@@ -432,20 +432,16 @@ void pdfview::content(const u32 page, const s32 X, const s32 Y,
 	Pixmap pix = XCreatePixmap(fl_display, fl_window, cur->w, cur->h, 24);
 	if (pix == None)
 		return;
-	XImage xi;
-	memset(&xi, 0, sizeof(XImage));
-	xi.format = ZPixmap;
-	xi.byte_order = ImageByteOrder(fl_display);
-	xi.depth = 24;
-	xi.bits_per_pixel = 24;
-	xi.width = cur->w;
-	xi.height = cur->h;
-	xi.data = (char *) cache[c];
-	xi.bytes_per_line = cur->w * 3;
+	XImage *xi = XCreateImage(fl_display, fl_visual->visual, 24, ZPixmap, 0,
+					(char *) cache[c],
+					cur->w, cur->h, 32, 0);
+	if (xi == NULL) die("xi null\n");
 
-	XPutImage(fl_display, pix, fl_gc, &xi, 0, 0, 0, 0, cur->w, cur->h);
+	XPutImage(fl_display, pix, fl_gc, xi, 0, 0, 0, 0, cur->w, cur->h);
 
 //	fl_draw_image(cache[c], X, Y, W, H, 3, file->cache[page].w * 3);
 
+	xi->data = NULL;
+	XDestroyImage(xi);
 	XFreePixmap(fl_display, pix);
 }
